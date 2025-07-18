953512eedb4f2ab27d35d2856c6c7d7c0db425c2
PTAE43 <kritsada1696@gmail.com>
2025-07-18 17:05:24 +0700

    Update ex-4.js
    Add , and ;

--- ex-4.js
+++ ex-4.js
@@ -3,8 +3,8 @@ let calculator = {
     add: (a,b) => a+b,
     subtract: (a,b) => a-b,
     multiply: (a,b) => a*b,
-    divide: (a,b) => a/b
-}
+    divide: (a,b) => a/b,
+};
 
 let addResult = calculator.add(10, 20);
 let divideResult = calculator.divide(3000, 10);
